6afd6663cc20c2fb2765a5aef83efdb4efea9ed0
Bill D. Pirkle <bill@mbpirkle.com>
2014-05-01 18:33:40 -0500

    Theme changes

---
+++ sites/all/themes/bdp_omega/templates/section--footer.tpl.php
@@ -0,0 +1,6 @@
+<footer<?php print $attributes; ?>>
+  <div id="copyright" class="container">
+    <p><a href="http://www.mbpirkle.com" class="bold">&copy; 2013 - <?php print(date("Y")); ?></a><span class="bold">&nbsp;&nbsp;Bill D. Pirkle.  All rights reserved.</span></p>
+  </div>
+  <?php /*print $content;*/ ?>
+</footer>
